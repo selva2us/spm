54c7bdf80a189a730725c5b989932f62b52e175f
selva2us <selva2us@gmail.com>
2022-11-25 20:40:19 +1300

    Initial Commit

--- src/main/java/com/ms/spm/security/SecurityConfig.java
+++ src/main/java/com/ms/spm/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/csv/**").permitAll()
+                .antMatchers("/api/goods/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
